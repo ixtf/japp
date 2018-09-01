c88b245b06b52870edc816a6c2dcdde8aaf8323d
jzb <ixtf1984@gmail.com>
2018-09-01 16:01:05 +0800

    Jvertx eb

--- vertx/src/main/java/com/github/ixtf/japp/vertx/Jvertx.java
+++ vertx/src/main/java/com/github/ixtf/japp/vertx/Jvertx.java
@@ -3,15 +3,20 @@ package com.github.ixtf.japp.vertx;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.ixtf.japp.core.exception.JException;
 import com.github.ixtf.japp.core.exception.JMultiException;
+import io.reactivex.CompletableObserver;
+import io.reactivex.SingleObserver;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.eventbus.Message;
 import io.vertx.reactivex.core.http.HttpServerResponse;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.handler.*;
 import io.vertx.reactivex.ext.web.sstore.SessionStore;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -20,6 +25,7 @@ import javax.validation.ValidatorFactory;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 import static com.github.ixtf.japp.core.Constant.ErrorCode;
@@ -28,9 +34,8 @@ import static com.github.ixtf.japp.core.Constant.MAPPER;
 /**
  * @author jzb 2018-08-17
  */
-@Slf4j
-public class Jvertx {
-    public static void enableCommon(Router router, SessionStore sessionStore) {
+public interface Jvertx {
+    static void enableCommon(Router router, SessionStore sessionStore) {
         Set<String> allowHeaders = new HashSet<>();
         allowHeaders.add("x-requested-with");
         allowHeaders.add("Access-Control-Allow-Origin");
@@ -52,12 +57,11 @@ public class Jvertx {
         router.route().handler(SessionHandler.create(sessionStore));
     }
 
-    public static void failureHandler(RoutingContext rc) {
+    static void failureHandler(RoutingContext rc) {
         final HttpServerResponse response = rc.response().setStatusCode(400);
         final Throwable failure = rc.failure();
         final JsonObject result = new JsonObject();
         // todo response content-type 为 json
-        log.error("", failure);
         if (failure instanceof JMultiException) {
             final JMultiException ex = (JMultiException) failure;
             final JsonArray errors = new JsonArray();
@@ -80,21 +84,26 @@ public class Jvertx {
         response.end(result.encode());
     }
 
-    public static <T> T readCommand(Class<T> clazz, RoutingContext rc) throws Exception {
+    static <T> T readCommand(Class<T> clazz, RoutingContext rc) throws Exception {
         return readCommand(clazz, rc.getBodyAsString());
     }
 
-    public static <T> T readCommand(Class<T> clazz, JsonNode node) throws Exception {
+    static <T> T readCommand(Class<T> clazz, JsonObject jsonObject) throws Exception {
+        final T command = jsonObject.mapTo(clazz);
+        return checkCommand(command);
+    }
+
+    static <T> T readCommand(Class<T> clazz, JsonNode node) throws Exception {
         final T command = MAPPER.convertValue(node, clazz);
         return checkCommand(command);
     }
 
-    public static <T> T readCommand(Class<T> clazz, String json) throws Exception {
+    static <T> T readCommand(Class<T> clazz, String json) throws Exception {
         final T command = MAPPER.readValue(json, clazz);
         return checkCommand(command);
     }
 
-    private static <T> T checkCommand(T command) throws JException {
+    static <T> T checkCommand(T command) throws JException {
         final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
         final Validator validator = validatorFactory.getValidator();
         final Set<ConstraintViolation<T>> violations = validator.validate(command);
@@ -111,5 +120,70 @@ public class Jvertx {
             throw exceptions.get(0);
         }
         throw new JMultiException(exceptions);
+    }
+
+    static <T> SingleObserver<T> toSingleObserver(Message reply) {
+        AtomicBoolean completed = new AtomicBoolean();
+        return new SingleObserver<T>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+            }
+
+            @Override
+            public void onSuccess(@NonNull T item) {
+                if (completed.compareAndSet(false, true)) {
+                    reply.reply(item);
+                }
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                if (completed.compareAndSet(false, true)) {
+                    handleError(reply, error);
+                }
+            }
+        };
+    }
+
+    static CompletableObserver toCompletableObserver(Message reply) {
+        AtomicBoolean completed = new AtomicBoolean();
+        return new CompletableObserver() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+            }
+
+            @Override
+            public void onComplete() {
+                if (completed.compareAndSet(false, true)) {
+                    reply.reply(null);
+                }
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                if (completed.compareAndSet(false, true)) {
+                    handleError(reply, error);
+                }
+            }
+        };
+    }
+
+    static void handleError(Message reply, Throwable error) {
+        LoggerFactory.getLogger(reply.address()).error("", error);
+        reply.fail(-1, error.getMessage());
+    }
+
+    interface EB {
+        static void noAction(Message reply) {
+            reply.fail(-1, "action not exist");
+        }
+
+        interface Header {
+            String action = "action";
+        }
+
+        interface Body {
+            String principal = "principal";
+        }
     }
 }
