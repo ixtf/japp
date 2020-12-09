ba34fc11b89984c5809d7a0134d434c5be49530c
ixtf <ixtf1984@gmail.com>
2020-12-09 15:33:52 +0800

    fix vertx

--- api/src/main/java/com/github/ixtf/api/ApiVerticle.java
+++ api/src/main/java/com/github/ixtf/api/ApiVerticle.java
@@ -7,7 +7,6 @@ import io.opentracing.Tracer;
 import io.opentracing.propagation.TextMapAdapter;
 import io.opentracing.tag.Tags;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
@@ -16,8 +15,7 @@ import io.vertx.core.eventbus.Message;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
-import io.vertx.ext.auth.oauth2.OAuth2ClientOptions;
-import io.vertx.ext.auth.oauth2.impl.OAuth2TokenImpl;
+import io.vertx.ext.auth.oauth2.OAuth2Options;
 import io.vertx.ext.bridge.PermittedOptions;
 import io.vertx.ext.healthchecks.HealthCheckHandler;
 import io.vertx.ext.healthchecks.HealthChecks;
@@ -50,53 +48,81 @@ public class ApiVerticle extends AbstractVerticle {
     @Inject
     private Optional<Tracer> tracerOpt;
     @Inject
-    private OAuth2ClientOptions oAuth2ClientOptions;
+    private OAuth2Options oAuth2Options;
     @Inject
     private CorsHandler corsHandler;
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
         injectMembers(this);
-        Future.<OAuth2Auth>future(p -> discover(vertx, oAuth2ClientOptions, p))
-                .map(OAuth2AuthHandler::create)
-                .flatMap(this::createHttpServer)
-                .<Void>mapEmpty()
-                .onComplete(startPromise);
+        discover(vertx, oAuth2Options).flatMap(this::createHttpServer).<Void>mapEmpty().onComplete(startPromise);
     }
 
-    private Future<HttpServer> createHttpServer(final OAuth2AuthHandler oauth2Handler) {
-        return Future.future(p -> {
-            final var router = Router.router(vertx);
-            router.route().handler(corsHandler);
-            router.route().handler(BodyHandler.create());
-            router.route("/metrics").handler(PrometheusScrapingHandler.create());
-            router.route("/health*").handler(HealthCheckHandler.create(vertx));
-            router.route("/ping*").handler(HealthCheckHandler.createWithHealthChecks(HealthChecks.create(vertx)));
+    private Future<HttpServer> createHttpServer(final OAuth2Auth oAuth2Auth) {
+        final var router = Router.router(vertx);
+        router.route().handler(corsHandler);
+        router.route().handler(BodyHandler.create());
+        router.route("/metrics").handler(PrometheusScrapingHandler.create());
+        router.route("/health*").handler(HealthCheckHandler.create(vertx));
+        router.route("/ping*").handler(HealthCheckHandler.createWithHealthChecks(HealthChecks.create(vertx)));
 
-            final var permitted = new PermittedOptions().setAddressRegex("medipath://ws/.+");
-            final var sockJSBridgeOptions = new SockJSBridgeOptions().addOutboundPermitted(permitted);
-            router.mountSubRouter("/eventbus", SockJSHandler.create(vertx).bridge(sockJSBridgeOptions));
+        final var permitted = new PermittedOptions().setAddressRegex("medipath://ws/.+");
+        final var sockJSBridgeOptions = new SockJSBridgeOptions().addOutboundPermitted(permitted);
+        router.mountSubRouter("/eventbus", SockJSHandler.create(vertx).bridge(sockJSBridgeOptions));
 
-            router.route("/api/services/:service/actions/:action").handler(oauth2Handler).handler(this::handleApi);
-            router.route("/dl/services/:service/actions/:action/tokens/:token").handler(this::handleDl);
+        router.route("/api/services/:service/actions/:action").handler(OAuth2AuthHandler.create(vertx, oAuth2Auth)).handler(this::handleApi);
+        router.route("/dl/services/:service/actions/:action/tokens/:token").handler(this::handleDl);
 
-            final var httpServerOptions = new HttpServerOptions().setCompressionSupported(true);
-            vertx.createHttpServer(httpServerOptions).requestHandler(router).listen(9998, p);
-        });
+        final var httpServerOptions = new HttpServerOptions().setCompressionSupported(true);
+        return vertx.createHttpServer(httpServerOptions).requestHandler(router).listen(9998);
     }
 
     private void handleApi(RoutingContext rc) {
         final var address = apiAddress(rc);
         final var spanOpt = spanOpt(rc, address);
         final var deliveryOptions = deliveryOptions(rc, spanOpt);
-        final var user = (OAuth2TokenImpl) rc.user();
-        final var accessToken = user.accessToken();
-        final var principal = accessToken.getString("sub");
+        final var principal = rc.user().attributes().getString("sub");
         deliveryOptions.addHeader(Principal.class.getName(), principal);
-        vertx.eventBus().request(address, rc.getBody(), deliveryOptions, ar -> {
-            rc.response().putHeader(CONTENT_TYPE, APPLICATION_JSON);
-            onComplete(rc, ar, spanOpt);
+        rc.response().putHeader(CONTENT_TYPE, APPLICATION_JSON);
+        vertx.eventBus().request(address, rc.getBody(), deliveryOptions)
+                .onSuccess(it -> this.onSuccess(rc, it, spanOpt))
+                .onFailure(e -> this.onFailure(rc, e, spanOpt));
+    }
+
+    private void onSuccess(RoutingContext rc, Message<Object> message, Optional<Span> spanOpt) {
+        final var response = rc.response();
+        message.headers().forEach(it -> {
+            if (it.getKey().equals(HttpResponseStatus.class.getName())) {
+                final var status = HttpResponseStatus.parseLine(it.getValue());
+                response.setStatusCode(status.code());
+            } else {
+                response.putHeader(it.getKey(), it.getValue());
+            }
         });
+        final var statusCode = response.getStatusCode();
+        if (statusCode >= 300 && statusCode < 400) {
+            response.end();
+        } else {
+            final var body = message.body();
+            if (body == null) {
+                response.end();
+            } else if (body instanceof Buffer) {
+                response.end((Buffer) body);
+            } else if (body instanceof byte[]) {
+                final var bytes = (byte[]) body;
+                response.end(Buffer.buffer(bytes));
+            } else if (body instanceof String) {
+                response.end((String) body);
+            } else {
+                onFailure(rc, new RuntimeException("body must be (null | Buffer | byte[] | String)"), spanOpt);
+            }
+        }
+    }
+
+    private void onFailure(RoutingContext rc, Throwable e, Optional<Span> spanOpt) {
+        rc.fail(e);
+        log.error(apiAddress(rc), e);
+        spanOpt.ifPresent(it -> it.setTag(Tags.ERROR, true).log(e.getMessage()));
     }
 
     private void handleDl(RoutingContext rc) {
@@ -107,10 +133,10 @@ public class ApiVerticle extends AbstractVerticle {
             deliveryOptions.setSendTimeout(Duration.ofMinutes(5).toMillis());
         }
         final var token = rc.pathParam("token");
-        vertx.eventBus().request(address, token, deliveryOptions, ar -> {
-            rc.response().putHeader(CONTENT_TYPE, APPLICATION_OCTET_STREAM);
-            onComplete(rc, ar, spanOpt);
-        });
+        rc.response().putHeader(CONTENT_TYPE, APPLICATION_OCTET_STREAM);
+        vertx.eventBus().request(address, token, deliveryOptions)
+                .onSuccess(it -> this.onSuccess(rc, it, spanOpt))
+                .onFailure(e -> this.onFailure(rc, e, spanOpt));
     }
 
     private String apiAddress(RoutingContext rc) {
@@ -152,56 +178,6 @@ public class ApiVerticle extends AbstractVerticle {
             }
         });
         return deliveryOptions;
-    }
-
-    private void onComplete(RoutingContext rc, AsyncResult<Message<Object>> ar, Optional<Span> spanOpt) {
-        try {
-            if (ar.failed()) {
-                onFailure(rc, ar.cause(), spanOpt);
-            } else {
-                onSuccess(rc, ar.result(), spanOpt);
-            }
-        } catch (Throwable e) {
-            onFailure(rc, e, spanOpt);
-        } finally {
-            spanOpt.ifPresent(Span::finish);
-        }
-    }
-
-    private void onSuccess(RoutingContext rc, Message<Object> message, Optional<Span> spanOpt) {
-        final var response = rc.response();
-        message.headers().forEach(it -> {
-            if (it.getKey().equals(HttpResponseStatus.class.getName())) {
-                final var status = HttpResponseStatus.parseLine(it.getValue());
-                response.setStatusCode(status.code());
-            } else {
-                response.putHeader(it.getKey(), it.getValue());
-            }
-        });
-        final var statusCode = response.getStatusCode();
-        if (statusCode >= 300 && statusCode < 400) {
-            response.end();
-        } else {
-            final var body = message.body();
-            if (body == null) {
-                response.end();
-            } else if (body instanceof Buffer) {
-                response.end((Buffer) body);
-            } else if (body instanceof byte[]) {
-                final var bytes = (byte[]) body;
-                response.end(Buffer.buffer(bytes));
-            } else if (body instanceof String) {
-                response.end((String) body);
-            } else {
-                onFailure(rc, new RuntimeException("body must be (null | Buffer | byte[] | String)"), spanOpt);
-            }
-        }
-    }
-
-    private void onFailure(RoutingContext rc, Throwable e, Optional<Span> spanOpt) {
-        rc.fail(e);
-        log.error(apiAddress(rc), e);
-        spanOpt.ifPresent(it -> it.setTag(Tags.ERROR, true).log(e.getMessage()));
     }
 
 }
