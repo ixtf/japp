d978d63621f923596ecda1c5bd28ec3fafde215c
ixtf <ixtf1984@gmail.com>
2021-11-13 00:00:26 +0800

    update vertx

--- api-vertx/src/main/java/com/github/ixtf/api/ws/SockJsEvent.java
+++ api-vertx/src/main/java/com/github/ixtf/api/ws/SockJsEvent.java
@@ -9,25 +9,35 @@ import static com.github.ixtf.guice.GuiceModule.getInstance;
 import static java.util.Optional.ofNullable;
 
 public class SockJsEvent implements Serializable {
-    private final String address;
-    private final String type;
-    private final Object payload;
+  private final String address;
+  private final String type;
+  private final Object payload;
 
-    public SockJsEvent(String address, String type, Object payload) {
-        this.address = address;
-        this.type = type;
-        this.payload = payload;
-    }
+  public SockJsEvent(String address, String type, Object payload) {
+    this.address = address;
+    this.type = type;
+    this.payload = payload;
+  }
 
-    public SockJsEvent(String address, String type) {
-        this(address, type, null);
-    }
+  public SockJsEvent(String address, String type) {
+    this(address, type, null);
+  }
 
-    public void publish() {
-        final var vertx = getInstance(Vertx.class);
-        final var data = new JsonObject().put("type", type);
-        ofNullable(payload).map(it -> it instanceof String ? it : JsonObject.mapFrom(it)).ifPresent(it -> data.put("payload", it));
-        vertx.eventBus().publish(address, data);
-    }
+  public void publish() {
+    final var vertx = getInstance(Vertx.class);
+    vertx.eventBus().publish(address, message());
+  }
 
+  private JsonObject message() {
+    final var ret = new JsonObject().put("type", type);
+    ofNullable(payload)
+        .map(it -> it instanceof String ? it : JsonObject.mapFrom(it))
+        .ifPresent(it -> ret.put("payload", it));
+    return ret;
+  }
+
+  public void send() {
+    final var vertx = getInstance(Vertx.class);
+    vertx.eventBus().send(address, message());
+  }
 }
