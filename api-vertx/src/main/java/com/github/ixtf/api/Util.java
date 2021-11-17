523d28f551375affb72960c582b10fbc5dbff925
ixtf <ixtf1984@gmail.com>
2021-11-17 21:50:59 +0800

    add graphql

--- api-vertx/src/main/java/com/github/ixtf/api/Util.java
+++ api-vertx/src/main/java/com/github/ixtf/api/Util.java
@@ -10,6 +10,7 @@ import io.opentracing.Tracer;
 import io.opentracing.propagation.TextMapAdapter;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
 import lombok.SneakyThrows;
 
 import java.io.File;
@@ -25,59 +26,77 @@ import static java.util.Optional.ofNullable;
 
 public class Util {
 
-    @SneakyThrows(IOException.class)
-    public static String loadGraphql(String s) {
-        final var url = Resources.getResource(s);
-        return Resources.toString(url, UTF_8);
-    }
+  public static String service(RoutingContext rc) {
+    return rc.pathParam("service");
+  }
 
-    public static <T> T checkAndGetCommand(DataFetchingEnvironment env, Class<T> clazz) {
-        final T command = MAPPER.convertValue(env.getArgument("command"), clazz);
-        return J.checkAndGetCommand(command);
-    }
+  public static String action(RoutingContext rc) {
+    return rc.pathParam("action");
+  }
 
-    public static Principal principal(DataFetchingEnvironment env) {
-        final var ctx = env.getGraphQlContext();
-        return new UserPrincipal(ctx.get(Principal.class.getName()));
-    }
+  public static String token(RoutingContext rc) {
+    return rc.pathParam("token");
+  }
 
-    public static <T> T checkAndGetCommand(Class<T> clazz, JsonObject jsonObject) {
-        return J.checkAndGetCommand(clazz, jsonObject.encode());
-    }
+  public static String address(RoutingContext rc) {
+    return String.join(":", service(rc), action(rc));
+  }
 
-    public static <T> T checkAndGetCommand(Class<T> clazz, JsonArray jsonArray) {
-        return J.checkAndGetCommand(clazz, jsonArray.encode());
-    }
+  @SneakyThrows(IOException.class)
+  public static String loadGraphql(String s) {
+    final var url = Resources.getResource(s);
+    return Resources.toString(url, UTF_8);
+  }
 
-    public static JsonObject config(final String env, final String defaultV) {
-        return ofNullable(System.getProperty(env))
-                .filter(J::nonBlank)
-                .or(() -> ofNullable(System.getenv(env)))
-                .filter(J::nonBlank)
-                .or(() -> ofNullable(defaultV))
-                .map(J::getFile)
-                .filter(File::exists)
-                .filter(File::canRead)
-                .map(J::readJson)
-                .map(JsonNode::toString)
-                .map(JsonObject::new)
-                .orElseGet(JsonObject::new);
-    }
+  public static <T> T checkAndGetCommand(DataFetchingEnvironment env, Class<T> clazz) {
+    final T command = MAPPER.convertValue(env.getArgument("command"), clazz);
+    return J.checkAndGetCommand(command);
+  }
 
-    public static Optional<Principal> principalOpt(final Map map) {
-        return ofNullable(map)
-                .map(it -> it.get(Principal.class.getName()))
-                .map(Object::toString)
-                .filter(J::nonBlank)
-                .map(UserPrincipal::new);
-    }
+  public static Principal principal(DataFetchingEnvironment env) {
+    final var ctx = env.getGraphQlContext();
+    return new UserPrincipal(ctx.get(Principal.class.getName()));
+  }
 
-    public static Optional<Span> spanOpt(final Optional<Tracer> tracerOpt, final String operationName, final Map map) {
-        return tracerOpt.map(tracer -> {
-            final var spanBuilder = tracer.buildSpan(operationName);
-            ofNullable(tracer.extract(TEXT_MAP, new TextMapAdapter(map))).ifPresent(spanBuilder::asChildOf);
-            return spanBuilder.start();
+  public static <T> T checkAndGetCommand(Class<T> clazz, JsonObject jsonObject) {
+    return J.checkAndGetCommand(clazz, jsonObject.encode());
+  }
+
+  public static <T> T checkAndGetCommand(Class<T> clazz, JsonArray jsonArray) {
+    return J.checkAndGetCommand(clazz, jsonArray.encode());
+  }
+
+  public static JsonObject config(final String env, final String defaultV) {
+    return ofNullable(System.getProperty(env))
+        .filter(J::nonBlank)
+        .or(() -> ofNullable(System.getenv(env)))
+        .filter(J::nonBlank)
+        .or(() -> ofNullable(defaultV))
+        .map(J::getFile)
+        .filter(File::exists)
+        .filter(File::canRead)
+        .map(J::readJson)
+        .map(JsonNode::toString)
+        .map(JsonObject::new)
+        .orElseGet(JsonObject::new);
+  }
+
+  public static Optional<Principal> principalOpt(final Map map) {
+    return ofNullable(map)
+        .map(it -> it.get(Principal.class.getName()))
+        .map(Object::toString)
+        .filter(J::nonBlank)
+        .map(UserPrincipal::new);
+  }
+
+  public static Optional<Span> spanOpt(
+      final Optional<Tracer> tracerOpt, final String operationName, final Map map) {
+    return tracerOpt.map(
+        tracer -> {
+          final var spanBuilder = tracer.buildSpan(operationName);
+          ofNullable(tracer.extract(TEXT_MAP, new TextMapAdapter(map)))
+              .ifPresent(spanBuilder::asChildOf);
+          return spanBuilder.start();
         });
-    }
-
+  }
 }
--- mongo/src/main/java/com/github/ixtf/mongo/BaseRepositoryJmongo.java
+++ mongo/src/main/java/com/github/ixtf/mongo/BaseRepositoryJmongo.java
@@ -16,102 +16,109 @@ import java.lang.reflect.ParameterizedType;
 
 import static java.util.Optional.ofNullable;
 
-public abstract class BaseRepositoryJmongo<T extends MongoEntityBase> implements RepositoryJmongo<T> {
-    @Getter(lazy = true, value = AccessLevel.PROTECTED)
-    private final LoadingCache<String, T> cache = _cacheBuilder().build(this::find);
-    protected final Class<T> entityClass = _entityClass();
-    @Inject
-    protected Jmongo jmongo;
+public abstract class BaseRepositoryJmongo<T extends MongoEntityBase>
+    implements RepositoryJmongo<T> {
+  protected final Class<T> entityClass = _entityClass();
+  @Inject protected Jmongo jmongo;
 
-    @Override
-    public T build(String id) {
-        Validate.notBlank(id);
-        return ofNullable(getCache().get(id)).orElseGet(() -> {
-            final var entity = create();
-            entity.setId(id);
-            return entity;
-        });
+  @Getter(lazy = true, value = AccessLevel.PROTECTED)
+  private final LoadingCache<String, T> cache = _cacheBuilder().build(this::find);
+
+  @Override
+  public T build(String id) {
+    Validate.notBlank(id);
+    return ofNullable(getCache().get(id))
+        .orElseGet(
+            () -> {
+              final var entity = create();
+              entity.setId(id);
+              return entity;
+            });
+  }
+
+  @Override
+  public void invalidateBuild(String id) {
+    Validate.notBlank(id);
+    getCache().invalidate(id);
+  }
+
+  @Override
+  public void invalidateBuild() {
+    getCache().invalidateAll();
+  }
+
+  @SneakyThrows({
+    NoSuchMethodException.class,
+    InvocationTargetException.class,
+    InstantiationException.class,
+    IllegalAccessException.class
+  })
+  @Override
+  public T create() {
+    final var entity = entityClass.getDeclaredConstructor().newInstance();
+    entity.setId(ObjectId.get().toHexString());
+    return entity;
+  }
+
+  @Override
+  public void insert(T entity) {
+    jmongo.uow().registerNew(entity).commit();
+    getCache().put(entity.getId(), entity);
+  }
+
+  @Override
+  public void update(T entity) {
+    jmongo.uow().registerDirty(entity).commit();
+    if (entity.isDeleted()) {
+      getCache().invalidate(entity.getId());
+    } else {
+      getCache().put(entity.getId(), entity);
     }
+  }
 
-    @Override
-    public void invalidateBuild(String id) {
-        Validate.notBlank(id);
-        getCache().invalidate(id);
-    }
+  @Override
+  public T find(String id) {
+    return jmongo.find(entityClass, id).block();
+  }
 
-    @Override
-    public void invalidateBuild() {
-        getCache().invalidateAll();
-    }
+  @Override
+  public Flux<T> list() {
+    return jmongo.list(entityClass);
+  }
 
-    @SneakyThrows({NoSuchMethodException.class, InvocationTargetException.class, InstantiationException.class, IllegalAccessException.class})
-    @Override
-    public T create() {
-        final var entity = entityClass.getDeclaredConstructor().newInstance();
-        entity.setId(ObjectId.get().toHexString());
-        return entity;
-    }
+  @Override
+  public void delete(T entity) {
+    entity.setDeleted(true);
+    jmongo.uow().registerDirty(entity).commit();
+    invalidateBuild(entity);
+  }
 
-    @Override
-    public void insert(T entity) {
-        jmongo.uow().registerNew(entity).commit();
-        getCache().put(entity.getId(), entity);
-    }
+  /**
+   * 真正删除
+   *
+   * @param entity entity
+   */
+  public void _delete(T entity) {
+    jmongo.uow().registerDelete(entity).commit();
+    invalidateBuild(entity);
+  }
 
-    @Override
-    public void update(T entity) {
-        jmongo.uow().registerDirty(entity).commit();
-        if (entity.isDeleted()) {
-            getCache().invalidate(entity.getId());
-        } else {
-            getCache().put(entity.getId(), entity);
-        }
-    }
+  @Override
+  public boolean exists(String id) {
+    return jmongo.exists(entityClass, id);
+  }
 
-    @Override
-    public T find(String id) {
-        return jmongo.find(entityClass, id).block();
-    }
+  @Override
+  public MongoCollection<T> entityCollection() {
+    return jmongo.entityCollection(entityClass);
+  }
 
-    @Override
-    public Flux<T> list() {
-        return jmongo.list(entityClass);
-    }
+  protected Caffeine<Object, Object> _cacheBuilder() {
+    return Caffeine.newBuilder();
+  }
 
-    @Override
-    public void delete(T entity) {
-        entity.setDeleted(true);
-        jmongo.uow().registerDirty(entity).commit();
-        invalidateBuild(entity);
-    }
-
-    /**
-     * 真正删除
-     *
-     * @param entity entity
-     */
-    public void _delete(T entity) {
-        jmongo.uow().registerDelete(entity).commit();
-        invalidateBuild(entity);
-    }
-
-    @Override
-    public boolean exists(String id) {
-        return jmongo.exists(entityClass, id);
-    }
-
-    @Override
-    public MongoCollection<T> entityCollection() {
-        return jmongo.entityCollection(entityClass);
-    }
-
-    protected Caffeine<Object, Object> _cacheBuilder() {
-        return Caffeine.newBuilder();
-    }
-
-    private final Class<T> _entityClass() {
-        final var parameterizedType = (ParameterizedType) this.getClass().getGenericSuperclass();
-        return (Class<T>) parameterizedType.getActualTypeArguments()[0];
-    }
-
+  private final Class<T> _entityClass() {
+    final var parameterizedType = (ParameterizedType) this.getClass().getGenericSuperclass();
+    return (Class<T>) parameterizedType.getActualTypeArguments()[0];
+  }
 }
