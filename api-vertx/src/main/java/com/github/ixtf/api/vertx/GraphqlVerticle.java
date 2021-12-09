91a777598bc43efd18c6a52a9a0713c1e43b51e3
ixtf <ixtf1984@gmail.com>
2021-12-09 23:14:11 +0800

    add graphql

--- api-vertx/src/main/java/com/github/ixtf/api/vertx/GraphqlVerticle.java
+++ api-vertx/src/main/java/com/github/ixtf/api/vertx/GraphqlVerticle.java
@@ -23,8 +23,11 @@ import io.vertx.ext.web.handler.graphql.impl.GraphQLBatch;
 import io.vertx.ext.web.handler.graphql.impl.GraphQLInput;
 import io.vertx.ext.web.handler.graphql.impl.GraphQLQuery;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import static com.github.ixtf.api.guice.ApiModule.GRAPHQL_ADDRESS;
 import static com.github.ixtf.guice.GuiceModule.injectMembers;
@@ -59,44 +62,67 @@ public class GraphqlVerticle extends AbstractVerticle implements Handler<Message
         try {
             final var graphQLInput = GraphQLInput.decode(reply.body());
             if (graphQLInput instanceof final GraphQLQuery query) {
-                final var deliveryOptions = new DeliveryOptions();
-                final var result = handleQuery(reply, query);
-                final var jsonObject = new JsonObject(result.toSpecification());
-                if (J.nonEmpty(result.getErrors())) {
-                    deliveryOptions.addHeader(HttpResponseStatus.class.getName(), "400");
-                }
-                reply.reply(jsonObject.toBuffer(), deliveryOptions);
-            } else if (graphQLInput instanceof final GraphQLBatch batch) {
-                final var deliveryOptions = new DeliveryOptions();
-                final var jsonArray = new JsonArray();
-                batch.forEach(query -> {
-                    final var result = handleQuery(reply, query);
-                    jsonArray.add(result.toSpecification());
-                    if (J.nonEmpty(result.getErrors())) {
-                        deliveryOptions.addHeader(HttpResponseStatus.class.getName(), "400");
+                handleQuery(reply, query).whenComplete((result,e)->{
+                    if (e == null) {
+                        final var deliveryOptions = new DeliveryOptions();
+                        final var jsonObject = new JsonObject(result.toSpecification());
+                        if (J.nonEmpty(result.getErrors())) {
+                            deliveryOptions.addHeader(HttpResponseStatus.class.getName(), "400");
+                        }
+                        reply.reply(jsonObject.toBuffer(), deliveryOptions);
+                    } else {
+                        onFail(reply,e,spanOpt);
                     }
                 });
-                reply.reply(jsonArray.toBuffer(), deliveryOptions);
+            } else if (graphQLInput instanceof final GraphQLBatch batch) {
+                Flux.fromIterable(batch)
+                        .flatMap(query -> {
+                            final var result = handleQuery(reply, query);
+                            return Mono.fromCompletionStage(result);
+                        })
+                        .collectList()
+                        .subscribe(list -> {
+                            final var deliveryOptions = new DeliveryOptions();
+                            final var jsonArray = new JsonArray();
+                            list.forEach(result -> {
+                                jsonArray.add(result.toSpecification());
+                                if (J.nonEmpty(result.getErrors())) {
+                                    deliveryOptions.addHeader(HttpResponseStatus.class.getName(), "400");
+                                }
+                            });
+                            reply.reply(jsonArray.toBuffer(), deliveryOptions);
+                        }, e -> onFail(reply,e,spanOpt));
             } else {
                 reply.fail(400, "no GraphQLInput");
             }
         } catch (Throwable e) {
-            final var errorMessage = e.getMessage();
-            reply.fail(400, errorMessage);
-            log.error("", e);
-            spanOpt.ifPresent(span -> span.setTag(Tags.ERROR, true).log(errorMessage).finish());
+            onFail(reply,e,spanOpt);
         } finally {
             spanOpt.ifPresent(Span::finish);
         }
     }
 
-    private ExecutionResult handleQuery(Message<Buffer> reply, GraphQLQuery query) {
-        return graphQL.execute(builder -> {
+    private void onFail(Message<Buffer> reply, Throwable e, Optional<Span> spanOpt) {
+        final var errorMessage = e.getMessage();
+        reply.fail(400, errorMessage);
+        log.error("", e);
+        spanOpt.ifPresent(span -> span.setTag(Tags.ERROR, true).log(errorMessage).finish());
+    }
+
+    private CompletableFuture<ExecutionResult> handleQuery(Message<Buffer> reply, GraphQLQuery query) {
+        return graphQL.executeAsync(builder -> {
             builder.query(query.getQuery());
             ofNullable(query.getOperationName()).filter(J::nonBlank).ifPresent(builder::operationName);
             ofNullable(query.getVariables()).ifPresent(builder::variables);
             builder.graphQLContext(ctx -> reply.headers().forEach(entry -> ctx.of(entry.getKey(), entry.getValue())));
             return builder;
         });
+//        return graphQL.execute(builder -> {
+//            builder.query(query.getQuery());
+//            ofNullable(query.getOperationName()).filter(J::nonBlank).ifPresent(builder::operationName);
+//            ofNullable(query.getVariables()).ifPresent(builder::variables);
+//            builder.graphQLContext(ctx -> reply.headers().forEach(entry -> ctx.of(entry.getKey(), entry.getValue())));
+//            return builder;
+//        });
     }
 }
